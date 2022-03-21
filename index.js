713eb5eb092f93fc47c9d04be01ba290700cd091
Natalie Thompson <Natalie.A.Thompson@outlook.com>
2022-03-21 16:22:52 -0500

    Created a get and post route for viewing and adding blogs

--- index.js
+++ index.js
@@ -10,8 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Main');
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+});
+
+
+app.post('/api/blogs', (request, response) => {
+  console.log(request.body)
+  const blog = new Blog(request.body)
+  console.log(blog)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 });
 
 const PORT = process.env.PORT;
